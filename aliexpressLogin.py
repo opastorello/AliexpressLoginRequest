093c6563effa49ff7c55d53d99d11c50a16a3863
Nícolas Pastorello <68306729+opastorello@users.noreply.github.com>
2021-07-14 20:50:59 -0300

    Add files via upload

---
+++ aliexpressLogin.py
@@ -0,0 +1,116 @@
+import random, requests, re, json, execjs
+
+def getRandomAgent():
+    USER_AGENTS = [
+        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)',
+        'Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)',
+        'Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)',
+        'Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)',
+        'Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)',
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)',
+        'Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6',
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1',
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0',
+        'Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5',
+        'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20',
+        'Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52',
+    ]
+    return USER_AGENTS[random.randint(0,9)]
+
+def get_umidToken_csrf_token(login_account):
+    url = 'https://passport.aliexpress.com/mini_login.htm?lang=pt_br&appName=aebuyer&appEntrance=default&styleType=auto&bizParams=&notLoadSsoView=false&notKeepLogin=true&isMobile=false&showMobilePwdLogin=false&defaultCountryCode=BR&ut=&rnd=0.9085151696364684'
+    
+    headers = {
+        'user-agent': getRandomAgent()
+    }
+    
+    data = {
+        'lang': 'pt_br',
+        'appName': 'aebuyer',
+        'appEntrance': 'default',
+        'styleType': 'auto',
+        'bizParams': '',
+        'notLoadSsoView': 'false',
+        'notKeepLogin': 'true',
+        'isMobile': 'false',
+        'loginId': login_account,
+        'showMobilePwdLogin': 'false',
+        'defaultCountryCode': 'BR',
+        'rnd': '0.9085151696364684',
+    }
+
+    s = requests.session()
+    try:
+        r = s.get(url, data=data, headers=headers)
+
+        response_string = r.text.encode('utf-8').decode('utf-8', 'ignore')
+        token_pattern = re.compile(r'window\.viewData\s+=\s+(.*?);')
+        result = token_pattern.findall(response_string,re.S)
+        resultJson = json.loads(result[0])
+
+        return resultJson['loginFormData']['umidToken'], resultJson['loginFormData']['_csrf_token']
+    except:
+        pass
+
+def get_password2(senha):
+    with open('mini_login.js', 'r', errors='ignore') as f:
+        js_code=f.read()
+    ctx = execjs.compile(js_code)
+    return ctx.call('getPwd', senha)
+
+def login(email, senha):
+    umidToken, csrf_token = get_umidToken_csrf_token(email)
+
+    url = 'https://passport.aliexpress.com/newlogin/login.do?appName=aebuyer&fromSite=13'
+
+    headers = {
+        'User-Agent': getRandomAgent(),
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': '2560',
+        'Origin': 'https://login.aliexpress.com',
+        'Connection': 'keep-alive',
+        'Referer': 'https://login.aliexpress.com/'
+    }
+
+    data = {
+        'loginId': email,
+        'password2': get_password2(senha),
+        'keepLogin': '',
+        'umidGetStatusVal': '255',
+        'screenPixel': '1366x768',
+        'navlanguage': 'pt-BR',
+        'navUserAgent': getRandomAgent(),
+        'navPlatform': 'Win32',
+        'appName': 'aebuyer',
+        'csrf_token': csrf_token,
+        'returnUrl': '',
+        'bizParams': '',
+        'appEntrance': 'aebuyer',
+        'isMobile': '',
+        'lang': 'pt_BR',
+        'fromSite': '13',
+        'umidToken': umidToken
+    }
+
+    s = requests.session()
+    try:
+        r = s.post(url, data=data, headers=headers)
+        if 'loginResult":"success' in r.text:
+            print('Logado com sucesso!')
+        elif 'Sua senha ou nome da conta está incorreto.' in r.text:
+            print('Sua senha ou nome da conta está incorreto')
+        elif 'isCheckCodeShowed":true' in r.text:
+            print('CAPTCHA solicitado!')
+        else:
+            print(r.text)
+    except:
+        pass
+
+login('email@gmail.com','password')
